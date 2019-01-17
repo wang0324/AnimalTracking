37cd8cd94f2f1bb193b2f9f7511b313a15f299b7
kwang627 <kevinwang0324@gmail.com>
2019-01-17 10:55:05 -0800

    removed unnecessary bw filter

--- src/AnimalTracker.java
+++ src/AnimalTracker.java
@@ -66,14 +66,7 @@ public class AnimalTracker implements PixelFilter {
         performSecondThreshold(out2);
 
         calculateCenterPoint(out2);
-        // we don't change the input image at all!
-//        short[][] blackPixels = new short[img.getHeight()][img.getWidth()];
-//
-//        for (int i = 0; i < img.getHeight(); i++) {
-//            for (int j= 0; j < img.getWidth(); j++) {
-//                blackPixels[i][j] = 100;
-//            }
-//        }
+        
         img.setPixels(out2);
 
         return img;
